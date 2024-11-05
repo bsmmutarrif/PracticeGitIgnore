bed8bd5666de801fab34e8d7746c914474c3c3ff
bsmmutarrif <mutarrif.nbr@gmail.com>
2024-11-05 16:51:24 +0600

    coding of concat by string

--- src/PracticeGitIgnore.java
+++ src/PracticeGitIgnore.java
@@ -1,7 +1,25 @@
 import java.util.Scanner;
+public class PracticeGitIgnore {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input
+        Scanner scanner = new Scanner(System.in);
 
-    public class PracticeGitIgnore {
-        public static void main(String[] args) {
-            // code to be added later
-        }
+        // Prompt the user to enter the first string
+        System.out.print("Enter the first string: ");
+        String str1 = scanner.nextLine();
+
+        // Prompt the user to enter the second string
+        System.out.print("Enter the second string: ");
+        String str2 = scanner.nextLine();
+
+        // Concatenate the two strings
+        String result = str1 + str2;
+
+        // Display the concatenated result
+        System.out.println("The concatenated string is: " + result);
+
+        // Close the scanner
+        scanner.close();
     }
+}
+
