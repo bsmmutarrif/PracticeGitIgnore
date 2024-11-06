30f684e035f883146dfd12d324a9d917a175583e
bsmmutarrif <mutarrif.nbr@gmail.com>
2024-11-06 11:06:26 +0600

    addding helper class

---
+++ src/Helper.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Helper {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String getInput(String prompt) {
+        System.out.print(prompt);
+
+        return null;
+    }
+
+    public static int getIntInput(String prompt) {
+        System.out.print(prompt);
+
+        return 0;
+    }
+    public static void closeScanner() {
+// Close the scanner if needed
+    }
+}
